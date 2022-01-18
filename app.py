7e88058ba21f6f467f06036add76a37cf5e3ae79
RakshithKodihalli <rakshith.gowda.kodihalli@gamil.com>
2022-01-18 23:51:04 +0530

    version 1

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app.layout = get_app_layout()
 
 def upload_data(contents, filename, date,  n_clicks, ip_Node):
     
-    if (n_clicks ==0) or (contents is None and n_clicks >0):
+    if (n_clicks ==0) or (contents is None and n_clicks > 0):
         edgesdf= pd.read_csv('data/Edges1_.csv')
         nodedf = pd.read_csv('data/Node1_.csv')
         mydata = networkgrapg(edgesdf , nodedf, ip_Node)
