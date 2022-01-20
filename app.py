6d56b479e22364365e9aa562fe81f0f77888d29b
RakshithKodihalli <rakshith.gowda.kodihalli@gamil.com>
2022-01-20 22:46:25 +0530

    altered for loop

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ def update(reset):
     
     
 if __name__ == '__main__':   
-    #app.run_server(debug=False)
-    app.run_server(debug=True)
+    app.run_server(debug=False)
+    #app.run_server(debug=True)
     
     
     
