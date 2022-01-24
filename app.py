b727fc10f8dbad997ba452346d12e47adee8faa7
RakshithKodihalli <rakshith.gowda.kodihalli@gamil.com>
2022-01-24 12:54:20 +0530

    adding port and and setting host 0.0.0.0

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ def update(reset):
     
     
 if __name__ == '__main__':   
-    #app.run_server(debug=False)
-    app.run_server(debug=True)
+    app.run_server(debug=False,host="0.0.0.0", port= "8055")
+    #app.run_server(debug=True)
     
     
     
